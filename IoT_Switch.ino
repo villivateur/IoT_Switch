8dadf57b0050fc8f8aeabe678c2ea8e3bbef4a24
Villivateur Von <villivateur@nuaa.edu.cn>
2018-01-08 20:20:34 +0800

    改了点错误

--- IoT_Switch.ino
+++ IoT_Switch.ino
@@ -42,14 +42,22 @@ void loop() {
         Serial.print("6");//发送一个“6”（测试用）
         delay(200);
         msglen = recvmsg();
+
         if (msg[2] == 'R' && msg[7] == '1' && msg[18] == 'S' && msg[23] == 'O' && msg[34] == '1' && msg[msglen - 2] == ':') {//同上比较
           digitalWrite(ErrorLED, 0);
-          if (msg[mslen] == '1') digitalWrite(MyLED, 1);
-          if (msg[mslen] == '0') digitalWrite(MyLED, 0);//根据回传数据控制
+          if (msg[msglen] == '1') digitalWrite(MyLED, 1);
+          if (msg[msglen] == '0') digitalWrite(MyLED, 0);//根据回传数据控制
         }
-        else digitalWrite(ErrorLED, 1);
+        else {
+          digitalWrite(ErrorLED, 1);
+          break;
+        }
+
       }
-      else digitalWrite(ErrorLED, 1);
+      else {
+        digitalWrite(ErrorLED, 1);
+        break;
+      }
     }
   }
   else digitalWrite(ErrorLED, 1);

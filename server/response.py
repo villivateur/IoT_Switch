920e781eb21d75478304f9d560c569f1869c253f
Villivateur Von <villivateur@nuaa.edu.cn>
2018-01-09 16:45:33 +0800

    修改python文件

--- server/response.py
+++ server/response.py
@@ -1,6 +1,8 @@
 import socket
+import os
+import re
 
-HOST, PORT = '', 10081
+HOST, PORT = '', 10082
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
@@ -10,6 +12,9 @@ while True:
     client_connection, client_address = listen_socket.accept()
     req = client_connection.recv(1024)
     print req
+    a = re.findall("name=(.+) ", req)
+    print a
     resp = "0"
+    os.system(a[0]) 
     client_connection.sendall(str(resp))
     client_connection.close()

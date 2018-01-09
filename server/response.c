885fde2533fb645786bdc258bf687db80e3a7c6e
Villivateur Von <villivateur@nuaa.edu.cn>
2018-01-09 16:33:34 +0800

    Revert "增加了连接的纠错，提高了稳定性"
    This reverts commit 30ff92b703c9d0aefe67a4e4f75dac17df9e5408.

--- server/response.c
+++ server/response.c
@@ -15,58 +15,58 @@
 
 int main()
 {
-	while (1)
+	int server_sockfd = socket(AF_INET,SOCK_STREAM, 0);
+
+
+	struct sockaddr_in server_sockaddr;
+	server_sockaddr.sin_family = AF_INET;
+	server_sockaddr.sin_port = htons(MYPORT);
+	server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+	if(bind(server_sockfd,(struct sockaddr *)&server_sockaddr,sizeof(server_sockaddr))==-1)
 	{
-		int server_sockfd = socket(AF_INET,SOCK_STREAM, 0);
-
-
-		struct sockaddr_in server_sockaddr;
-		server_sockaddr.sin_family = AF_INET;
-		server_sockaddr.sin_port = htons(MYPORT);
-		server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-		if(bind(server_sockfd,(struct sockaddr *)&server_sockaddr,sizeof(server_sockaddr))==-1)
-		{
-			perror("bind");
-		}
-
-		///listen，成功返回0，出错返回-1
-		if(listen(server_sockfd,QUEUE) == -1)
-		{
-			perror("listen");
-		}
-
-		///客户端套接字
-		char buffer[BUFFER_SIZE];
-		struct sockaddr_in client_addr;
-		socklen_t length = sizeof(client_addr);
-
-		///成功返回非负描述字，出错返回-1
-		int conn = accept(server_sockfd, (struct sockaddr*)&client_addr, &length);
-		if(conn<0)
-		{
-			perror("connect");
-		}
-
-		while(1)
-		{
-			memset(buffer,0,sizeof(buffer));
-			int len = recv(conn, buffer, sizeof(buffer),0);	  
-			//printf("recive message from client:\n");
-			//fputs(buffer,stdout);
-			if (buffer[0]!='6') break;
-			//printf("\n");
-			FILE *ctrl=fopen("ctrl.in","r");
-			buffer[0]=fgetc(ctrl);
-			fclose(ctrl);
-			//buffer[0]='1';
-			//printf("Send messsge to client:\n");
-			//fputs(buffer,stdout);
-			//printf("\n"); 
-			send(conn, buffer, len, 0);
-		}
-		close(conn);
-		close(server_sockfd);
+		perror("bind");
+		exit(1);
 	}
+
+	///listen，成功返回0，出错返回-1
+	if(listen(server_sockfd,QUEUE) == -1)
+	{
+		perror("listen");
+		exit(1);
+	}
+
+	///客户端套接字
+	char buffer[BUFFER_SIZE];
+	struct sockaddr_in client_addr;
+	socklen_t length = sizeof(client_addr);
+
+	///成功返回非负描述字，出错返回-1
+	int conn = accept(server_sockfd, (struct sockaddr*)&client_addr, &length);
+	if(conn<0)
+	{
+		perror("connect");
+		exit(1);
+	}
+
+	while(1)
+	{
+		memset(buffer,0,sizeof(buffer));
+		int len = recv(conn, buffer, sizeof(buffer),0);	  
+		//printf("recive message from client:\n");
+		//fputs(buffer,stdout);
+		//printf("\n");
+		FILE *ctrl=fopen("ctrl.in","r");
+		buffer[0]=fgetc(ctrl);
+		fclose(ctrl);
+		//buffer[0]='1';
+		//printf("Send messsge to client:\n");
+		//fputs(buffer,stdout);
+		//printf("\n"); 
+		send(conn, buffer, len, 0);
+	}
+	close(conn);
+	close(server_sockfd);
 	return 0;
 }
